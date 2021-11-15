96e8ce469f1f3d02938602a2fb143a135d66518e
DavidPgl <panagiotopulos.d@gmail.com>
2021-11-15 23:56:45 +0100

    Generalize CameraScaler

--- Assets/Scripts/Util/CameraScaler.cs
+++ Assets/Scripts/Util/CameraScaler.cs
@@ -3,29 +3,33 @@ using UnityEngine;
 [ExecuteAlways]
 public class CameraScaler : MonoBehaviour
 {
-    private void Awake()
-    {
-        ScaleCamera();
-    }
+    [SerializeField] private Camera cameraToScale;
+    [SerializeField] private Vector2 worldSize = new Vector2(10,20);
+
+    private void Awake() => ScaleCamera(worldSize);
+
 #if UNITY_EDITOR
-    private void Update()
-    {
-        ScaleCamera();
-    }
+    private void Update() => ScaleCamera(worldSize);
 #endif
 
-
-    private void ScaleCamera()
+    /// <summary>
+    /// Scales the orthographic size of the camera so it's always showing the defined world size.
+    /// </summary>
+    public void ScaleCamera(Vector2 worldSize)
     {
+        if (cameraToScale == null)
+            return;
+
         float aspectRatio = (float)Screen.height / Screen.width;
 
-        if (aspectRatio >= (float)Constants.GRID_HEGIHT / Constants.GRID_WIDTH)
+        // Either set to max width or max height
+        if (aspectRatio >= (float)worldSize.y / worldSize.x)
         {
-            Camera.main.orthographicSize = aspectRatio * Constants.GRID_WIDTH / 2;
+            cameraToScale.orthographicSize = aspectRatio * worldSize.x / 2;
         }
         else
         {
-            Camera.main.orthographicSize = Constants.GRID_HEGIHT / 2;
+            cameraToScale.orthographicSize = worldSize.y / 2;
         }
 
     }
