ac232300010560b77e3aab5c4cbfd5d97417f0ab
DavidPgl <panagiotopulos.d@gmail.com>
2021-11-16 17:33:58 +0100

    Add margin to camera scaler

--- Assets/Scripts/Util/CameraScaler.cs
+++ Assets/Scripts/Util/CameraScaler.cs
@@ -1,36 +1,57 @@
 using UnityEngine;
 
-[ExecuteAlways]
-public class CameraScaler : MonoBehaviour
+namespace Util
 {
-    [SerializeField] private Camera cameraToScale;
-    [SerializeField] private Vector2 worldSize = new Vector2(10,20);
+#if UNITY_EDITOR
+    [ExecuteAlways]
+#endif
+    public class CameraScaler : MonoBehaviour
+    {
+        [SerializeField] private Camera cameraToScale;
 
-    private void Awake() => ScaleCamera(worldSize);
+        [SerializeField] [Tooltip("The size of the world the camera needs to display in world units")]
+        private Vector2 worldSize = new Vector2(10, 20);
+
+        [SerializeField] [Tooltip("The margin placed on the top in world units")]
+        private float marginTop = 0.0f;
+
+        [SerializeField] [Tooltip("The margin placed on the bottom in world units")]
+        private float marginBottom = 0.0f;
+
+        [SerializeField] [Tooltip("The margin placed on the left in world units")]
+        private float marginLeft = 0.0f;
+
+        [SerializeField] [Tooltip("The margin placed on the right in world units")]
+        private float marginRight = 0.0f;
+
+        private void Awake() => ScaleCamera();
 
 #if UNITY_EDITOR
-    private void Update() => ScaleCamera(worldSize);
+        private void Update() => ScaleCamera();
 #endif
 
-    /// <summary>
-    /// Scales the orthographic size of the camera so it's always showing the defined world size.
-    /// </summary>
-    public void ScaleCamera(Vector2 worldSize)
-    {
-        if (cameraToScale == null)
-            return;
-
-        float aspectRatio = (float)Screen.height / Screen.width;
-
-        // Either set to max width or max height
-        if (aspectRatio >= (float)worldSize.y / worldSize.x)
+        /// <summary>
+        /// Scales the orthographic size of the camera so it's always showing the defined world size.
+        /// </summary>
+        private void ScaleCamera()
         {
-            cameraToScale.orthographicSize = aspectRatio * worldSize.x / 2;
-        }
-        else
-        {
-            cameraToScale.orthographicSize = worldSize.y / 2;
-        }
+            if (cameraToScale == null)
+                return;
 
+            float aspectRatio = (float)Screen.height / Screen.width;
+
+            // Either set to max width or max height
+            if (aspectRatio >= (worldSize.y + marginTop + marginBottom) / (worldSize.x + marginLeft + marginRight))
+            {
+                cameraToScale.orthographicSize = aspectRatio * (worldSize.x + marginLeft + marginRight) / 2.0f;
+            }
+            else
+            {
+                cameraToScale.orthographicSize = (worldSize.y + marginTop + marginBottom) / 2;
+            }
+
+            cameraToScale.transform.position = new Vector3(worldSize.x / 2.0f + (marginRight - marginLeft) / 2.0f,
+                worldSize.y / 2.0f + (marginTop - marginBottom) / 2.0f, -10.0f);
+        }
     }
 }
